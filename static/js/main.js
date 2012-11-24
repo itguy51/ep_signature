f0f943ac9ca37b36e6adb942535bbd438138e52e
Josh Pruim <josh@josh-M11x-R2.(none)>
2012-11-23 21:58:06 -0800

    Added Templating

--- static/js/main.js
+++ static/js/main.js
@@ -2,17 +2,40 @@ $(document).ready(function (){
 	$('#insertSignature a').on("click", function(e){
 		if(!pad.getUserName() || pad.getUserName() == "undefined"){
 			$("#myusernameedit").val(prompt("Please enter your name, then try again.",""));
-			e = jQuery.Event("keypress")
-			e.which = 13 //choose the one you want
+			e = jQuery.Event("keypress");
+			e.which = 13;
     		$("#myusernameedit").keypress().trigger(e)​;
 		}else{
 			var d = new Date();
 			var offset = (d.getTimezoneOffset()/60)*(-1);
-			if(offset == 0){
-				offset = "";
+			if(offset > 0){
+				offset = "+" + offset;
 			}
-			var signature = " -- " + pad.getUserName() + " " + d.getFullYear() + "-" + ("0" + d.getMonth()).slice(-2) + "-" + ("0" + d.getDay()).slice(-2) + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + " UTC+" + offset;
-
+			//Modified for the UTC+(Number) so that we get UTC-(Number) instead of UTC+-(Number)
+			//Offset now contains a prefix (+/-)
+			var signature = "-- %u% %y%-%m%-%d% %h%:%m% UTC%o%";
+			/* As it sits, this is the current key for the replacer:
+			 * %u% - The Username of the current person
+			 * %y% - The Year of the current timezone
+			 * %m% - The Month of the current timezone
+			 * %d% - The Day of the current timezone
+			 * %h% - The Hour of the current timezone
+			 * %m% - The Minutes of the current timezone
+			 * %s% - The Seconds of the current timezone
+			 * %o% - The current timezone offset of UTC
+			 */
+			//Notes - Is regexing the fastest method here? Opera and FF seem to go faster on a mystring.split('.').join(' ');
+			//But the performance increase is minor, so no more work unless it becomes more proven.
+			signature = signature.replace(/%u%/g, pad.getUserName());
+			signature = signature.replace(/%y%/g, d.getFullYear());
+			signature = signature.replace(/%m%/g, ("0" + d.getMonth()).slice(-2));
+			signature = signature.replace(/%d%/g, ("0" + d.getDay()).slice(-2));
+			signature = signature.replace(/%h%/g, ("0" + d.getHours()).slice(-2));
+			signature = signature.replace(/%m%/g, ("0" + d.getMinutes()).slice(-2));
+			signature = signature.replace(/%s%/g, ("0" + d.getSeconds()).slice(-2));
+			signature = signature.replace(/%o%/g, offset);
+			//var signature = " -- " + pad.getUserName() + " " + d.getFullYear() + "-" + ("0" + d.getMonth()).slice(-2) + "-" + ("0" + d.getDay()).slice(-2) + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + " UTC+" + offset;
+			//Commented Out in preparation for replacement by the above functions
 			var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
 
 			return padeditor.ace.callWithAce(function (ace) {
